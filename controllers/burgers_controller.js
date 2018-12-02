aaef50b5d3d2283cce5624d091ff112635211bb1
thirteen <41817587+maguirre13@users.noreply.github.com>
2018-12-02 14:02:05 -0800

    update burgers_controller.js

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,16 @@
+var express = require("express");
+
+var router = express.Router();
+var burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+  res.redirect("/burgers");
+});
+
+router.get("/burgers", function(req, res) {
+  burger.all(function(burgerData) {
+    resnrender("index", { burger_data: burgerData });
+  });
+});
+
+module.exports = router;
