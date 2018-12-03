d6036a50ccb7c8a32adfb3fdec3e33a1f1430912
thirteen <41817587+maguirre13@users.noreply.github.com>
2018-12-02 22:31:33 -0800

    update seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheese Burger', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Avacado Bacon Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Avocado Bacon Burger', FALSE);
